1d36e9f671acfa822287e5708bca2ec8d4b5cc65
Munif Tanjim <muniftanjim@gmail.com>
2020-05-28 02:56:09 +0600

    feat: add basic template util

---
+++ src/utils/template.ts
@@ -0,0 +1,25 @@
+export function template(
+  literals: TemplateStringsArray,
+  ...keys: Array<number | string>
+) {
+  return (
+    values: Array<number | string> | { [key: string]: number | string }
+  ): string => {
+    const dict: Record<string, number | string> = Array.isArray(values)
+      ? values.reduce((dict: Record<string, number | string>, value, index) => {
+          dict[index] = value
+          return dict
+        }, {})
+      : values
+
+    const parts = keys.reduce<string[]>((parts, key, index) => {
+      const value = `${dict[key]}`
+      parts.push(literals[index], value)
+      return parts
+    }, [])
+
+    parts.push(literals[literals.length - 1])
+
+    return parts.join('')
+  }
+}
